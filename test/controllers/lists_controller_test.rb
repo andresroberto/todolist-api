bd265c4e7369cb67de985c052af633a0300ba676
Andrés Rojas <andresroberto10@gmail.com>
2024-10-11 14:19:56 +0200

    Add tests to lists controller

--- test/controllers/lists_controller_test.rb
+++ test/controllers/lists_controller_test.rb
@@ -3,11 +3,15 @@ require "test_helper"
 class ListsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @list = lists(:one)
+    @task = tasks(:one)
   end
 
   test "should get index" do
     get lists_url, as: :json
     assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert_not_empty json_response
   end
 
   test "should create list" do
@@ -18,14 +22,31 @@ class ListsControllerTest < ActionDispatch::IntegrationTest
     assert_response :created
   end
 
+  test "should not create list with invalid name" do
+    assert_no_difference('List.count') do
+      post lists_url, params: { list: { name: '' } }, as: :json
+    end
+
+    assert_response :unprocessable_entity
+  end
+
   test "should show list" do
     get list_url(@list), as: :json
     assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert_equal @list.id, json_response["id"]
+    assert_not_nil json_response["tasks"]
   end
 
   test "should update list" do
     patch list_url(@list), params: { list: { name: @list.name } }, as: :json
     assert_response :success
+  end
+
+  test "should not update list with invalid name" do
+    patch list_url(@list), params: { list: { name: '' } }, as: :json
+    assert_response :unprocessable_entity
   end
 
   test "should destroy list" do
